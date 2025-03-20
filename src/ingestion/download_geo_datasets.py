947f4df57777f103a420f3949d3665b4529d074c
Momir Milutinović <milutinm462@gmail.com>
2025-03-20 13:48:00 +0100

    refactor: Extract fetch_geo_accessions function into its own file

--- src/ingestion/download_geo_datasets.py
+++ src/ingestion/download_geo_datasets.py
@@ -6,6 +6,7 @@ import re
 from fetch_geo_ids import fetch_geo_ids
 from rate_limit import check_limit
 from soft_metadata_line_iterator import metadata_line_iterator
+from fetch_geo_accessions import fetch_geo_accessions
 from GEOparse.utils import smart_open
 
 
@@ -38,27 +39,6 @@ def download_geo_dataset(accession: str) -> GEODataset:
         return GEODataset(metadata)
 
     return GEODataset(dataset)
-
-
-def fetch_geo_accessions(ids: List[str]) -> List[str]:
-    """
-    Fetches GEO accessions for the given GEO IDs.
-
-    :param geo_ids: GEO dataset IDs for which to fetch accessions.
-    :return: List of GEO acessions in the same order.
-    """
-    check_limit()
-    geo_summaries = str(
-        requests.get(
-            "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
-            {"db": "gds", "id": ",".join(map(str, ids))},
-        ).content
-    )
-
-    # Series are the only type of GEO entry that contain all of the infromation
-    # we are looking for. Therefore we need to search for series accessions,
-    # which begin with GSE.
-    return re.findall("Accession: (GSE\\d+)", geo_summaries)
 
 
 if __name__ == "__main__":
---
+++ src/ingestion/fetch_geo_accessions.py
@@ -0,0 +1,24 @@
+import requests
+from typing import List
+from rate_limit import check_limit
+
+
+def fetch_geo_accessions(ids: List[str]) -> List[str]:
+    """
+    Fetches GEO accessions for the given GEO IDs.
+
+    :param geo_ids: GEO dataset IDs for which to fetch accessions.
+    :return: List of GEO acessions in the same order.
+    """
+    check_limit()
+    geo_summaries = str(
+        requests.get(
+            "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
+            {"db": "gds", "id": ",".join(map(str, ids))},
+        ).content
+    )
+
+    # Series are the only type of GEO entry that contain all of the infromation
+    # we are looking for. Therefore we need to search for series accessions,
+    # which begin with GSE.
+    return re.findall("Accession: (GSE\\d+)", geo_summaries)

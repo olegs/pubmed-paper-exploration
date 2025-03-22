df6ec24c2dbead799ffbecec8559d4a3be8c7ad2
Momir Milutinović <milutinm462@gmail.com>
2025-03-22 22:33:56 +0100

    fix Remove the summary and overall_design fields from the string representations of SuperSeries GEODatasets
    This is done because these fields do not contain any meaningful
    information when the dataset is a SuperSeries

--- src/model/geo_dataset.py
+++ src/model/geo_dataset.py
@@ -12,7 +12,13 @@ class GEODataset:
         self.pubmed_ids: List[str] = metadata.get("pubmed_id", [])
 
     def __str__(self):
-        return f"{self.title}\n{self.experiment_type}\n{self.summary}\n{','.join(self.organisms)}\n{self.overall_design}"
+        if self.is_not_superseries():
+            return f"{self.title}\n{self.experiment_type}\n{self.summary}\n{','.join(self.organisms)}\n{self.overall_design}"
+        else:
+            # The summary and overall design fields for SuperSeries do not contain any useful information.
+            # (Their content always is "This SuperSeries is composed of the SubSeries listed below." and "Refer to individual Series")
+            # When this content is not removed, all SuperSeries get grouped into cluster despite being about vastly different topics.
+            return f"{self.title}\n{self.experiment_type}\n{','.join(self.organisms)}"
 
     def __eq__(self, other):
         return (

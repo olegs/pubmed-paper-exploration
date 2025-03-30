ef9bb41650928757d5a8274d938de63a3ad029b3
Momir Milutinović <milutinm462@gmail.com>
2025-03-30 17:13:53 +0200

    feat: Check if clustering.topic_words is configured to be at least 5

--- src/config/config.py
+++ src/config/config.py
@@ -7,6 +7,8 @@ class Config:
         self._config.read(config_path)
         self.svd_dimensions = self._config.getint("clustering", "svd_dimensions")
         self.topic_words = self._config.getint("clustering", "topic_words")
+        if self.topic_words < 5:
+            raise ValueError("clustering.topic_words must be greater than or equal to 5. Please check the configuration.")
         self.download_folder = self._config["ingestion"]["download_folder"]
         self.loglevel = self._config["logging"]["log_level"]
 
